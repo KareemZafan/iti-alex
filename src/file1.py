be6aaab40c779bada496d95c77ed17488a6f0af4
KareemZafan <kareemm.zafan@gmail.com>
2025-10-04 12:39:22 +0300

    Adding a new test-python file

---
+++ src/file1.py
@@ -0,0 +1,18 @@
+impot pytest
+
+def add(a,b):
+ return a + b
+ 
+def mul(a,b):
+ return a + b
+ 
+def div(a,b):
+ if b == 0: 
+  return None
+ else 
+  return a /b 
+
+
+# todo write tests for the previous functions
+ 
+
